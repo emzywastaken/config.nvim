b23ed6eec9c4043143b88268250a281b83b1783a
emzywastaken <amiamemetoo@gmail.com>
2025-08-22 00:04:40 +0100

    Clean init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
 require 'config.options'
 require 'core.lazy'
--- require 'core.lsp'
 require 'config.keymaps'
 require 'config.autocmds'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
