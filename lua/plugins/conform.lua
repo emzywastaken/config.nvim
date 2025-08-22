a639f97d4eab457dd1da1302407382adfa3bf108
emzywastaken <amiamemetoo@gmail.com>
2025-08-22 12:59:47 +0100

    Add TODO in conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,6 +18,7 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style.
       local disable_filetypes = { c = true, cpp = true }
+      -- TODO: Send some sort of notification for disabled ft
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
