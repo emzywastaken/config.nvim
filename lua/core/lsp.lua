8bb9a65855d976e8bb22ddd01c50f20239763d36
emzywastaken <amiamemetoo@gmail.com>
2025-08-22 16:57:47 +0100

    Enable nixd lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -105,6 +105,7 @@ return { -- Main LSP Configuration
     local capabilities = require('blink.cmp').get_lsp_capabilities()
     -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
     local servers = {
+      nixd = {},
       clangd = {},
       -- gopls = {},
       rust_analyzer = {},
