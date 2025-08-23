3007d276836f5b78d990929e61fc2cdb25d2daf4
emzywastaken <amiamemetoo@gmail.com>
2025-08-23 13:59:41 +0100

    Add keymap to toggle background transparency

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,13 +3,24 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
+    local transparent = false
+
     require('tokyonight').setup {
-      -- transparent = true,
+      transparent = transparent,
       styles = {
         comments = { italic = false }, -- Disable italics in comments
       },
     }
 
+    local toggle_bg = function()
+      transparent = not transparent
+      require('tokyonight').setup {
+        transparent = transparent,
+      }
+      vim.cmd.colorscheme 'tokyonight-night'
+    end
+
+    vim.keymap.set('n', '<leader>tb', toggle_bg, { desc = 'Toggle Background Transparency' })
     vim.cmd.colorscheme 'tokyonight-night'
   end,
 }
