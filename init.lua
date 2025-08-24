ae73ceee9757ed3c9ab5c45482121a707d054fa4
emzywastaken <amiamemetoo@gmail.com>
2025-08-24 15:22:32 +0100

    yada yada yada

--- init.lua
+++ init.lua
@@ -2,3 +2,7 @@ require 'config.options'
 require 'core.lazy'
 require 'config.keymaps'
 require 'config.autocmds'
+
+-- https://github.com/neovim/neovim/issues/34945
+-- Fucking lsp hover doesn't close when you change buffers.
+-- The fix has been merged though :)
