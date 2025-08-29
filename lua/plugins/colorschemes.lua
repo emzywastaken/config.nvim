b45b7f12e5a0e8f4acc6553f5507fcb5074c2abd
emzywastaken <amiamemetoo@gmail.com>
2025-08-29 23:06:23 +0100

    Set default transparency to true

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    local transparent = false
+    local transparent = true
 
     require('tokyonight').setup {
       transparent = transparent,
