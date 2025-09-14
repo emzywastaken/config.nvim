24cf9742e4ee7cfb7ad9c0228fb34de7612be62e
emzywastaken <amiamemetoo@gmail.com>
2025-09-14 23:03:32 +0100

    remove dead comments

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -147,12 +147,6 @@ return { -- Main LSP Configuration
       },
     }
 
-    -- local ensure_installed = vim.tbl_keys(servers or {})
-    -- vim.list_extend(ensure_installed, {
-    --   'stylua', -- Used to format Lua code
-    -- })
-    -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
     for server, cfg in pairs(servers) do
       cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
       vim.lsp.config(server, cfg)
