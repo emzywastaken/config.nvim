f06e88cc19bdd508047f45c8edf3e0eafad82d61
emzywastaken <amiamemetoo@gmail.com>
2025-12-23 16:56:55 +0100

    dont show bufferline when theres only a single buffer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,5 +6,9 @@ return {
     { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
   },
-  opts = {},
+  opts = {
+    options = {
+      always_show_bufferline = false,
+    },
+  },
 }
