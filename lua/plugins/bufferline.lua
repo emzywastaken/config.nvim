1f95908dba42bb85c22fca48f0e7314f7e423adf
emzywastaken <amiamemetoo@gmail.com>
2025-12-23 16:42:28 +0100

    bind `<S-h>` and `<S-l>` to next buffer and previous buffer respectively

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,10 @@
 return {
   'akinsho/bufferline.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
+  event = 'VeryLazy',
+  keys = {
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+  },
   opts = {},
 }
